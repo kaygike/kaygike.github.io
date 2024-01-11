5d86483dcfb8983150861b3700449bcad340a277
0x646a73 <0x646a73@proton.me>
2024-01-10 22:39:14 -0600

    add year & build date variables to metadata

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 #
 # Convert pages/*.md to /*.html using custom stylesheet & template
 
-# List of filenames (without extension)
+# List of filenames in pages/ (without .md extension)
 declare -a arr=("index" "books" "code" "other" "quotes" "text")
 
 for i in "${arr[@]}"
@@ -16,7 +16,8 @@ do
     --highlight-style=kate \
     --css=include/style.css \
     --template=include/html-template.pandoc \
-    --metadata \
-    title="$i" \
+    --metadata title="$i" \
+    --metadata year="$(date +%Y)" \
+    --metadata build="$(date +%Y-%m-%d)"\
     pages/$i.md
 done
