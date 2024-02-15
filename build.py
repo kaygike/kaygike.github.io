c164ddfe50c0a86bb6495fcd094d104e6c95d175
0x646a73 <0x646a73@proton.me>
2024-02-14 22:51:59 -0600

    copy inc/style.css to publish dir; add vars to templating engine

--- build.py
+++ build.py
@@ -3,6 +3,8 @@
 
 # Standard
 import datetime
+import os
+import shutil
 # External
 from jinja2 import Environment, fileSystemLoader
 # Internal
@@ -10,12 +12,20 @@ from modules import config
 
 
 if __name == '__main__':
+    # Copy CSS to publish directory.
+    # Makes it easier to deploy on Netlify, Render, etc. since host won't need
+    # to access parent directory, e.g. ('../inc/style.css')
+    css_basename = os.path.basename(config.CSS_PATH)
+    destination = os.path.join(config.PUBLISH_DIR, css_basename)
+    shutil.copyfile(config.CSS_PATH, destination)
 
     # Initialize template system and write HTML to file
     env = Environment(loader=FileSystemLoader(config.TEMPLATES_DIR))
     template = env.get_template(config.TEMPLATE)
     content = template.render(
-        title = title,
+        site = config.SITE_NAME,
+        desc = config.SITE_DESC,
+        title = page_title,
         toc = toc,
         body = body,
         template = config.TEMPLATE,
