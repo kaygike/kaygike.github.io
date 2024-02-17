210cfd15660725b8c8c3ec052141cb48dacf80f4
0x646a73 <0x646a73@proton.me>
2024-02-16 21:43:10 -0600

    begin writing pages/*.md loop - not working/pseudocode

--- build.py
+++ build.py
@@ -4,31 +4,41 @@
 # Standard
 import datetime
 import os
+import pathlib
 import shutil
 # External
 from jinja2 import Environment, FileSystemLoader
+import markdown
 # Internal
 from modules import config
 
 
 if __name__ == '__main__':
     # Copy CSS to publish directory.
-    # Makes it easier to deploy on Netlify, Render, etc. since host 
+    # Makes it easier to deploy on Netlify, Render, etc. since host
     # won't need to access parent directory, e.g. ('../inc/style.css')
-    includes_css = os.path.join(config.INCLUDES_DIR, config.CSS_FILE)
     publish_css = os.path.join(config.PUBLISH_DIR, config.CSS_FILE)
-    shutil.copyfile(includes_css, publish_css)
+    shutil.copyfile(config.CSS_FILE, publish_css)
 
     # Initialize template system and write HTML to file
     env = Environment(loader=FileSystemLoader(config.TEMPLATES_DIR))
-    template = env.get_template(config.TEMPLATE)
-    content = template.render(
-        site = config.SITE_NAME,
-        desc = config.SITE_DESC,
-        title = page_title,
-        nav = config.NAV,
-        toc = toc,
-        body = body,
-        template = config.TEMPLATE,
-        now = datetime.datetime.now()
-    )
+
+    # Iterate through *.md files in pages/ and render HTML
+    pages = pathlib.Path(config.PAGES_DIR)
+    for inode in pages.iterdir():
+        if inode.is_dir():
+            print(config.PAGES_DIR, 'subdirectories not supported.')
+        else:
+            md = markdown.markdown(**CONTENT**, extensions=['meta'])
+
+            template = env.get_template(config.TEMPLATE)
+            content = template.render(
+                site = config.SITE_NAME,
+                desc = config.SITE_DESC,
+                title = page_title,
+                nav = config.NAV,
+                toc = toc,
+                body = body,
+                template = config.TEMPLATE,
+                now = datetime.datetime.now()
+            )
