616da605abc95c732a6e940a9b416a84df666806
0x646a73 <0x646a73@proton.me>
2024-02-18 22:38:53 -0600

    chg comments, build out loop; NOT WORKING

--- build.py
+++ build.py
@@ -5,20 +5,22 @@
 import datetime
 import os
 import pathlib
+import pprint
 import shutil
 # External
 from jinja2 import Environment, FileSystemLoader
+import frontmatter
 import markdown
 # Internal
 from modules import config
 
 
 if __name__ == '__main__':
-    # Copy CSS to publish directory.
-    # Makes it easier to deploy on Netlify, Render, etc. since host
-    # won't need to access parent directory, e.g. ('../inc/style.css')
-    publish_css = os.path.join(config.PUBLISH_DIR, config.CSS_FILE)
-    shutil.copyfile(config.CSS_FILE, publish_css)
+    # Copy CSS file from 'includes' dir to 'public' dir
+    # (So hosts won't need to access parent directory)
+    inc_css = os.path.join(config.INCLUDES_DIR, config.CSS_FILE)
+    pub_css = os.path.join(config.PUBLISH_DIR, config.CSS_FILE)
+    shutil.copyfile(inc_css, pub_css)
 
     # Initialize template system and write HTML to file
     env = Environment(loader=FileSystemLoader(config.TEMPLATES_DIR))
@@ -29,16 +31,29 @@ if __name__ == '__main__':
         if inode.is_dir():
             print(config.PAGES_DIR, 'subdirectories not supported.')
         else:
-            md = markdown.markdown(**CONTENT**, extensions=['meta'])
+            with open(inode, 'r', encoding='utf-8') as f:
+                # Read *.md frontmatter and content
+                data = f.read()
+                fm = frontmatter.loads(data)
+                mdown = markdown.markdown(data, extensions=['meta'])
+                
+                page_title = fm.get('title', inode)
+                pprint.pprint(page_title)
 
-            template = env.get_template(config.TEMPLATE)
-            content = template.render(
-                site = config.SITE_NAME,
-                desc = config.SITE_DESC,
-                title = page_title,
-                nav = config.NAV,
-                toc = toc,
-                body = body,
-                template = config.TEMPLATE,
-                now = datetime.datetime.now()
-            )
+                template = env.get_template(config.TEMPLATE)
+                content = template.render(
+                    site = config.SITE_NAME,
+                    desc = config.SITE_DESC,
+                    title = page_title,
+                    nav = config.NAV,
+                    #toc = toc,
+                    body = mdown,
+                    template = config.TEMPLATE,
+                    now = datetime.datetime.now()
+                )
+
+
+                fname = pathlib.Path(inode).stem + ".html"
+                fpath = config.PUBLISH_DIR + os.path.basename(fname)
+                with open(fpath, 'w', encoding='utf-8') as f:
+                    f.write(content)
